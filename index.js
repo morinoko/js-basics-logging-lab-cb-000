13b287e5f701caa2439eee39f8c21fa9909ce3f1
Felice Forby <feliceforby@gmail.com>
2018-12-20 23:14:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const driver = 'bob';
 
+// Pass strings into console log
 console.log('this code was called');
 
+// Pass variables into console log
 console.log(driver);
+
+// Pass multiple arguments into console log
+console.log(driver, '')
